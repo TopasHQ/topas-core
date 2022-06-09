22a845379e563fcefa680c1b1bf4c43784f0e2cc
lemii <25596204+Lemii@users.noreply.github.com>
2022-06-09 12:11:25 +0200

    randomize monster attributes

--- src/app/modules/monsters/monsters_module.ts
+++ src/app/modules/monsters/monsters_module.ts
@@ -18,7 +18,13 @@ import { AfterBlockApplyContext, AfterGenesisBlockApplyContext, BaseModule, code
 import config from '../../config';
 import { ModuleId, ModuleName, Monster, MonstersModuleChainData } from '../../types';
 import { parseChainData } from '../../utils/formats';
-import { bufferToHex } from '../../utils/helpers';
+import {
+    bufferToHex,
+    chunkSubstr,
+    getPseudoRandomBigIntInRange,
+    getPseudoRandomNumberInRange,
+    popFromArray,
+} from '../../utils/helpers';
 import { getDataAccessData, getStateStoreData } from '../../utils/store';
 import { DestroyMonsterAsset } from './assets/destroy_monster_asset';
 import { MONSTERS_INIT, MONSTERS_KEY, monstersModuleSchema } from './schemas';
@@ -46,18 +52,29 @@ export class MonstersModule extends BaseModule {
 
 	public async afterBlockApply({ stateStore, block }: AfterBlockApplyContext) {
 		if (block.header.height % config.monsterSpawnRate !== 0) {
-			this._logger.debug('Monster not spawned.');
+			this._logger.debug('Monster not spawned. Block height does not match spawn rate.');
 			return;
 		}
+		const id = bufferToHex(block.header.id);
+		const seeds = chunkSubstr(id); // Use the block id to generate multiple 'random' seeds
 
 		const stateStoreData = await getStateStoreData<MonstersModuleChainData>(stateStore, ModuleId.Monsters);
 
-		const id = bufferToHex(block.header.id);
+		const locationsTaken = stateStoreData.activeMonsters.map(monster => monster.location);
+		const locationsAvailable = Array.from(Array(config.monsterLocations).keys()).filter(
+			location => !locationsTaken.includes(location),
+		);
+
+		if (!locationsAvailable.length) {
+			this._logger.debug('Monster not spawned. All locations taken.');
+			return;
+		}
+
 		const monster: Monster = {
 			id,
-			model: 1,
-			location: 1,
-			reward: BigInt('1'),
+			model: getPseudoRandomNumberInRange(0, config.monsterModels - 1, popFromArray(seeds)),
+			location: locationsAvailable[getPseudoRandomNumberInRange(0, locationsAvailable.length + 1, popFromArray(seeds))],
+			reward: getPseudoRandomBigIntInRange(config.monsterMinReward, config.monsterMaxReward, popFromArray(seeds)),
 		};
 
 		stateStoreData.activeMonsters.push(monster);
--- src/app/utils/helpers.ts
+++ src/app/utils/helpers.ts
@@ -1,5 +1,6 @@
 import { Transaction, transactions } from 'lisk-sdk';
 import { DateTime } from 'luxon';
+import * as seedrandom from 'seedrandom';
 
 import {
     Highscore,
@@ -58,3 +59,49 @@ export const senderOwnsApp = (app: TopasApp, account: TopasAppModuleAccountProps
 	account.topasApp.appsPurchased.map(a => a.id).includes(app.data.id);
 
 export const bufferToHex = (input: Buffer) => input.toString('hex');
+
+// Inspired by: https://stackoverflow.com/a/29202760
+export const chunkSubstr = (str: string, size = 8) => {
+	const numChunks = Math.ceil(str.length / size);
+	const chunks = new Array(numChunks);
+
+	for (let i = 0, o = 0; i < numChunks; i += 1, o += size) {
+		chunks[i] = str.substr(o, size);
+	}
+
+	return chunks as string[];
+};
+
+export const getPseudoRandomFloat = (seed: string): number => {
+	const rng = seedrandom(seed);
+	return rng();
+};
+
+export const getPseudoRandomNumberInRange = (min: number, max: number, seed: string): number => {
+	const rng = getPseudoRandomFloat(seed);
+	return Math.floor(rng * (max - min + 1)) + min;
+};
+
+export const getPseudoRandomBigIntInRange = (minBigInt: bigint, maxBigInt: bigint, seed: string): bigint => {
+	const multiplier = BigInt('100000000');
+
+	const min = Number(minBigInt / multiplier);
+	const max = Number(maxBigInt / multiplier);
+
+	const rng = getPseudoRandomFloat(seed);
+	const randomNumber = Math.floor(rng * (max - min + 1)) + min;
+
+	return BigInt(randomNumber) * multiplier;
+};
+
+export const getRandomArrayElement = <T>(array: T[]): T => array[Math.floor(Math.random() * array.length)];
+
+export const popFromArray = <T>(array: T[]) => {
+	const element = array.pop();
+
+	if (!element) {
+		throw new Error('Array is empty');
+	}
+
+	return element;
+};
