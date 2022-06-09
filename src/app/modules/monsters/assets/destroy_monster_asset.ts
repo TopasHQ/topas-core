098b79ba0464709c5f164a7f425768bef442acf4
lemii <25596204+Lemii@users.noreply.github.com>
2022-06-09 13:32:19 +0200

    add destroy monster asset

--- src/app/modules/monsters/assets/destroy_monster_asset.ts
+++ src/app/modules/monsters/assets/destroy_monster_asset.ts
@@ -1,24 +1,52 @@
-import { ApplyAssetContext, BaseAsset, ValidateAssetContext } from 'lisk-sdk';
+import { ApplyAssetContext, BaseAsset, codec, ValidateAssetContext } from 'lisk-sdk';
+
+import { ModuleId, MonstersModuleChainData } from '../../../types';
+import { getStateStoreData } from '../../../utils/store';
+import { validateHexString, validateRegistration } from '../../../utils/validation';
+import { MONSTERS_ASSET_IDS } from '../constants';
+import { MONSTERS_KEY, monstersModuleSchema } from '../schemas';
+
+type Props = {
+	id: string;
+};
 
 export class DestroyMonsterAsset extends BaseAsset {
 	public name = 'destroyMonster';
-	public id = 1;
+	public id = MONSTERS_ASSET_IDS.destroyMonster;
 
-	// Define schema for asset
 	public schema = {
 		$id: 'monsters/destroyMonster-asset',
 		title: 'DestroyMonsterAsset transaction asset for monsters module',
 		type: 'object',
-		required: [],
-		properties: {},
+		required: ['id'],
+		properties: {
+			id: {
+				dataType: 'string',
+				fieldNumber: 1,
+			},
+		},
 	};
-
-	public validate({ asset }: ValidateAssetContext<{}>): void {
-		// Validate your asset
+	public validate({ asset }: ValidateAssetContext<Props>): void {
+		validateHexString(asset.id);
 	}
 
-	// eslint-disable-next-line @typescript-eslint/require-await
-	public async apply({ asset, transaction, stateStore }: ApplyAssetContext<{}>): Promise<void> {
-		throw new Error('Asset "destroyMonster" apply hook is not implemented.');
+	public async apply({ asset, transaction, stateStore, reducerHandler }: ApplyAssetContext<Props>): Promise<void> {
+		await validateRegistration(reducerHandler, transaction.senderAddress);
+		const stateStoreData = await getStateStoreData<MonstersModuleChainData>(stateStore, ModuleId.Monsters);
+
+		const monster = stateStoreData.activeMonsters.find(m => m.id === asset.id);
+
+		if (!monster) {
+			throw Error('Monster does not exist.');
+		}
+
+		stateStoreData.activeMonsters = stateStoreData.activeMonsters.filter(m => m.id !== asset.id);
+
+		await reducerHandler.invoke('token:credit', {
+			address: transaction.senderAddress,
+			amount: monster.reward,
+		});
+
+		await stateStore.chain.set(MONSTERS_KEY, codec.encode(monstersModuleSchema, stateStoreData));
 	}
 }
