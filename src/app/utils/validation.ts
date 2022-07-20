965dff7827fc8d708e1766bd4d11e9734e5f1c89
lemii <25596204+Lemii@users.noreply.github.com>
2022-07-20 12:54:23 +0200

    add validation to post score asset

--- src/app/utils/validation.ts
+++ src/app/utils/validation.ts
@@ -1,10 +1,12 @@
 import { ReducerHandler, Transaction, validator } from 'lisk-sdk';
+import { DateTime } from 'luxon';
 import { validate as uuidValidate, version as uuidVersion } from 'uuid';
 
 import config from '../config';
 import { TICKER } from '../constants';
 import { TopasApp } from '../modules/topas_app/types';
-import { beddowsToLsk } from './helpers';
+import { TopasAppPurchase } from '../types';
+import { beddowsToLsk, getDiffInMinutes } from './helpers';
 import { getTopasUserData } from './reducer_handlers';
 
 export const validateUuid = (uuid: string) => {
@@ -60,4 +62,12 @@ export const validateIsPublished = (app: TopasApp) => {
 // Attempts to fetch TopasUser data. Throws error when empty.
 export const validateRegistration = async (reducerHandler: ReducerHandler, address: Buffer) => {
 	await getTopasUserData(reducerHandler, { address, errorOnEmpty: true });
+};
+
+export const validatePurchaseDate = (purchase: TopasAppPurchase) => {
+	const diff = getDiffInMinutes(DateTime.fromSeconds(purchase.createdAt.unix));
+
+	if (diff > config.appPurchaseDuration) {
+		throw new Error(`Invalid score - purchase ID is too old.`);
+	}
 };
