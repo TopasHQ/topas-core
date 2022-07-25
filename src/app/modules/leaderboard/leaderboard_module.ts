9ba94b9dacfd5a701beb326445a1489fced08ea9
lemii <25596204+Lemii@users.noreply.github.com>
2022-07-25 13:09:31 +0200

    reset leaderboard after duration has been exceeded

--- src/app/modules/leaderboard/leaderboard_module.ts
+++ src/app/modules/leaderboard/leaderboard_module.ts
@@ -1,5 +1,6 @@
-import { AfterGenesisBlockApplyContext, BaseModule, codec } from 'lisk-sdk';
+import { AfterBlockApplyContext, AfterGenesisBlockApplyContext, BaseModule, codec } from 'lisk-sdk';
 
+import config from '../../config';
 import { ModuleId, ModuleName } from '../../types';
 import { getDataAccessData } from '../../utils/store';
 import { PostScoreAsset } from './assets/post_score_asset';
@@ -25,6 +26,19 @@ export class LeaderboardModule extends BaseModule {
 		getHighscores: async (): Promise<Highscore[]> =>
 			(await getDataAccessData<LeaderboardModuleChainData>(this._dataAccess, ModuleId.Leaderboard)).highscores,
 	};
+
+	public async afterBlockApply({ stateStore, block }: AfterBlockApplyContext) {
+		// Logic to reset leaderboard
+		if (block.header.height % config.appLeaderboardDuration !== 0) {
+			return;
+		}
+
+		const stateStoreData = await getDataAccessData<LeaderboardModuleChainData>(this._dataAccess, ModuleId.Leaderboard);
+		stateStoreData.highscores = [];
+		await stateStore.chain.set(LEADERBOARD_MODULE_KEY, codec.encode(leaderboardModuleSchema, stateStoreData));
+
+		this._logger.info('App leaderboard duration reached; highscores have been reset');
+	}
 
 	public async afterGenesisBlockApply(_input: AfterGenesisBlockApplyContext) {
 		await _input.stateStore.chain.set(
